7463dad276e70f4ba6846cbf22aa13ca6b7d6dd3
EAdamg <adamgalm@gmail.com>
2023-05-05 23:41:17 +0100

    FluxAndMonoGeneratorService

--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,4 +1,13 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+
 public class FluxAndMonoGeneratorServiceTest {
+
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFLux() {
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+    }
 }
