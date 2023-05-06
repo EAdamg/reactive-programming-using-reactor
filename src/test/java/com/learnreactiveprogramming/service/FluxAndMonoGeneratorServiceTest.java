1d65d06df1275abbe2876d359fb124f3db76cab1
Adam Galmor <adamgalm@gmail.com>
2023-05-06 15:09:10 +0100

    FluxAndMonoGeneratorServiceTest

--- src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,13 +1,40 @@
 package com.learnreactiveprogramming.service;
 
 import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoGeneratorServiceTest {
 
     FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
     @Test
-    void namesFLux() {
+    void namesFlux() {
         var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("alex", "ben", "chloe")
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesMono() {
+        var namesMono = fluxAndMonoGeneratorService.namesMono();
+
+        StepVerifier.create(namesMono)
+                .expectNextCount(1)
+                .verifyComplete();
+
+        StepVerifier.create(namesMono)
+                .expectNext("alex")
+                .verifyComplete();
     }
 }
